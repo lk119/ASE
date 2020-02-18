5cc9179d0de0e5751f96bd348b2031c6b7ae42e9
km184 <55809503+km184@users.noreply.github.com>
2020-02-18 23:37:31 +0000

    Flight Class skeleton with comparators

---
+++ FlightClasses/src/advancedJava/FlightCarrierComparator.java
@@ -0,0 +1,18 @@
+
+package advancedJava;
+
+/**
+ * @author km184
+ *
+ */
+import java.util.Comparator;
+
+public class FlightCarrierComparator implements Comparator<Flight> {
+
+	@Override
+	public int compare(Flight o1, Flight o2) {
+
+		return o1.getCarrier().compareToIgnoreCase(o2.getCarrier());
+	}
+
+}
