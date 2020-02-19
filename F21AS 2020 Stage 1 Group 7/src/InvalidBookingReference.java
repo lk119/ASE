bfceeef634df067877e52130e65063a734aaee52
rrp3 <55551866+RachPatel@users.noreply.github.com>
2020-02-19 18:15:03 +0000

    Integrated Code stage 1

---
+++ F21AS 2020 Stage 1 Group 7/src/InvalidBookingReference.java
@@ -0,0 +1,10 @@
+
+public class InvalidBookingReference extends Exception {
+	 
+	public void MyException(String s) 
+	    { 
+	        // Call constructor of parent Exception 
+	        System.out.println("Invalid characters in booking reference please use the form XX123456"); 
+	    } 
+
+}
