ecc91c41b664c46662c50cc217801becb54cfcb2
Lynsey Kirk <55623711+lk119@users.noreply.github.com>
2020-02-21 23:13:40 +0000

    Add files via upload

---
+++ TPGuiTest/src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+	
+	public static void main(String[] arg) {
+		
+//		TPBaggage gui = new TPBaggage();
+//		gui.setVisible(true);
+		
+		TPDetails gui2 = new TPDetails();
+		gui2.setVisible(true);
+		
+	}
+
+}
