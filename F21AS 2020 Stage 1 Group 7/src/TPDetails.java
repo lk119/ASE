30c5bded208cf5a37b9f39eea8cd1acf27208ba7
Lynsey Kirk <55623711+lk119@users.noreply.github.com>
2020-02-22 11:43:42 +0000

    added functionality
    reformatted for encapsulation

--- F21AS 2020 Stage 1 Group 7/src/TPDetails.java
+++ F21AS 2020 Stage 1 Group 7/src/TPDetails.java
@@ -1,144 +1,196 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
-
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.*;
-import javax.swing.text.JTextComponent;
 
-import lekrws1rrp3Assignment2.Competitor;
+public class TPDetails extends JFrame implements ActionListener {
 
+	private PassengerSet passengerSet;
+	private BaggageList baggageList;
 
-public class TPDetails extends JFrame {
 	private JTextField bookingRefField, lastNameField;
 	private JButton submitButton;
 	private ImageIcon boardingPass;
 	private JLabel imagePin;
-	
-	public TPDetails() {
+
+	public TPDetails(PassengerSet passengers) {
+		this.passengerSet = passengers;
+
+		// set up window title
+
+		setupNorthPanel();
+		setupCenterPanel();
+		setupSouthPanel();
+
+		setTitle("TravelPigeon Check-in");
 		getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		setSize(600,500);
-		
-		// setup north panel
-		
+		setSize(600, 500);
+
+		pack();
+		setVisible(true);
+		setLocationRelativeTo(null); // sets position of JFrame to middle of screen
+
+		// disable standard close button
+		setDefaultCloseOperation(this.DO_NOTHING_ON_CLOSE);
+		// show JOptionpane confirmation when close button pressed
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				int confirmed = JOptionPane.showConfirmDialog(null,
+						"Are you sure you want to exit? You're check-in is incomplete, you will not be checked-in.",
+						"Exit Programme", JOptionPane.YES_NO_OPTION);
+
+				if (confirmed == JOptionPane.YES_OPTION) {
+					dispose();
+				}
+			}
+		});
+	}
+
+	// setup north panel
+	private void setupNorthPanel() {
 		JPanel northPanel = new JPanel();
 		northPanel.setBackground(new Color(255, 255, 255));
 		northPanel.setForeground(Color.CYAN);
-		
-		
-		getContentPane().add(northPanel, BorderLayout.NORTH);
 		northPanel.setLayout(new GridLayout(0, 3, 0, 0));
-		
+
 		JPanel panel = new JPanel();
-		northPanel.add(panel);
 		panel.setBackground(Color.WHITE);
 		panel.setLayout(new GridLayout(0, 3, 0, 0));
-		
+
 		JLabel label = new JLabel("Please Enter Your Details Below:");
-		northPanel.add(label);
 		label.setForeground(new Color(0, 206, 209));
 		label.setBackground(Color.WHITE);
-		
-		// setup center panel
-		
+
+		northPanel.add(panel);
+		northPanel.add(label);
+
+		getContentPane().add(northPanel, BorderLayout.NORTH);
+	}
+
+	// setup center panel
+	private void setupCenterPanel() {
 		JPanel centerPanel = new JPanel();
 		centerPanel.setBackground(new Color(255, 255, 255));
-		getContentPane().add(centerPanel, BorderLayout.CENTER);
 		centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-			// setupDetailsPanel
+
+		// setupDetailsPanel
 		JPanel detailsPanel = new JPanel();
 		detailsPanel.setBackground(Color.WHITE);
-		centerPanel.add(detailsPanel);
 		detailsPanel.setLayout(new GridLayout(0, 3, 0, 0));
-		
+
 		JLabel bookingRefLabel = new JLabel("Booking Reference");
 		bookingRefLabel.setForeground(new Color(0, 206, 209));
-		detailsPanel.add(bookingRefLabel);
-		
+
 		bookingRefField = new JTextField();
 		bookingRefField.setToolTipText("Enter your Booking reference here");
 		bookingRefField.setColumns(1);
-		detailsPanel.add(bookingRefField);
-		
-		JLabel spacerLabel = new JLabel("");
-		detailsPanel.add(spacerLabel);
-		
+
 		JLabel lastNameLabel = new JLabel("Last Name");
 		lastNameLabel.setForeground(new Color(0, 206, 209));
-		detailsPanel.add(lastNameLabel);
-		
+
 		lastNameField = new JTextField();
 		lastNameField.setToolTipText("Enter your last name here");
 		lastNameField.setColumns(1);
 		lastNameField.setBackground(Color.WHITE);
-		detailsPanel.add(lastNameField);
-		
+
+		JLabel spacerLabel = new JLabel("");
 		JLabel spacerLabel2 = new JLabel("");
+
+		detailsPanel.add(bookingRefLabel);
+		detailsPanel.add(bookingRefField);
+		detailsPanel.add(spacerLabel);
+		detailsPanel.add(lastNameLabel);
+		detailsPanel.add(lastNameField);
 		detailsPanel.add(spacerLabel2);
-		
-			// setup imagePanel
+
+		// setup imagePanel
 		JPanel imagePanel = new JPanel();
-		centerPanel.add(imagePanel);
 		imagePanel.setBackground(Color.WHITE);
 		imagePanel.setLayout(new GridLayout(0, 3, 0, 0));
-		
+
 		boardingPass = new ImageIcon(getClass().getResource("Tic2.png"));
 		imagePin = new JLabel(boardingPass);
-		
+
 		JPanel spacerPanel = new JPanel();
 		spacerPanel.setBackground(Color.WHITE);
+
 		imagePanel.add(spacerPanel);
-
-
 		imagePanel.add(imagePin);
-		
-		
-		// setup south panel
-		
+
+		centerPanel.add(detailsPanel);
+		centerPanel.add(imagePanel);
+
+		getContentPane().add(centerPanel, BorderLayout.CENTER);
+	}
+
+	// setup south panel
+	private void setupSouthPanel() {
 		JPanel southPanel = new JPanel();
 		southPanel.setBackground(new Color(255, 255, 255));
-		getContentPane().add(southPanel, BorderLayout.SOUTH);
-		
+
 		submitButton = new JButton("Submit");
 		submitButton.setForeground(new Color(0, 206, 209));
+		submitButton.addActionListener(this);
+
 		southPanel.add(submitButton);
-		
-		
-		submitButton.addActionListener(new ActionListener() {
 
-			public void actionPerformed(ActionEvent e) {
-				TPBaggage TPBaggage = new TPBaggage();
-				TPBaggage.setVisible(true);
-				TPBaggage.setLocationRelativeTo(null);
-				dispose();
-			}
-		});
-	
+		getContentPane().add(southPanel, BorderLayout.SOUTH);
 	}
-	/**
-	 * Identifies the user's input and searches for the passenger last name and booking reference number.
-	 * 
-	 */
-	private void search() {
-		JTextComponent searchField;
-		// get input and trim to remove additional spaces
-		String searchString = searchField.getText().trim();
 
-		// check the input is has a value
-		if (searchString.length() > 0) {
-
-			
-			String bookingReferenceNum = searchString;
-			
-					
-			// return the passenger object
-			Passenger p = PassengerSet.findBookingRefNum(bookingReferenceNum);
-			
-			
-
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == submitButton) {
+			search();
+			this.dispose();
 		}
 	}
-}
 
+	private void search() {
+		// get input and trim to remove additional spaces
+		String bookingRef = getBookingRef();
+		String bookingName = getBookingName();
+
+		// check the input has a value
+		if (bookingRef.length() > 0) {
+			if (bookingName.length() > 0) {
+
+				Passenger p;
+				try {
+					p = passengerSet.findBooking(bookingRef, bookingName);
+					if (p != null) {
+						runTPBaggage();
+					}
+				} catch (NoMatchingBookingReference e) {
+					JOptionPane.showMessageDialog(null, "Booking Reference not found");
+				} catch (InvalidBookingReference e) {
+					JOptionPane.showMessageDialog(null, "Invalid Booking Reference. Please use the form XX123456");
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Please enter your Last Name");
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Please enter your Booking Reference");
+		}
+	}
+
+	public void runTPBaggage() {
+		TPBaggage baggage = new TPBaggage(baggageList);
+		baggage.setBookingReference(getBookingRef());
+		baggage.setBookingName(getBookingName());
+		baggage.setVisible(true);
+		baggage.setLocationRelativeTo(null);
+		System.out.println(baggage.getBookingReference());
+		System.out.println(baggage.getBookingLastName());
+	}
+
+	private String getBookingRef() {
+		String bookingRef = bookingRefField.getText().trim();
+		return bookingRef;
+	}
+
+	private String getBookingName() {
+		String bookingName = lastNameField.getText().trim();
+		return bookingName;
+	}
+}
