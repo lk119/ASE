6d821083cd52c5182f3d243cb4f9d11337796a2a
rws1 <55705431+rws1@users.noreply.github.com>
2020-02-25 14:12:45 +0000

    TPUTest3.0

--- TPUTest.java
+++ TPUTest.java
@@ -1,11 +1,14 @@
 import java.lang.*;
+import java.util.HashSet;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
 public class TPUTest {
 
+	
 	@Test
 	public void testgetFullName() {
 		 String expected1 = "Robert William Stone";
@@ -13,7 +16,7 @@ public class TPUTest {
 	     String actual1 = name1.getFullName();
 		 assertEquals(expected1, actual1);
 		 
-		 String expected2 = "Robert Stone";
+		 String expected2 = "Robert  Stone";
 		 Name name2 = new Name("Robert","","Stone");
 	     String actual2 = name2.getFullName();
 		 assertEquals(expected2, actual2);
@@ -28,4 +31,60 @@ public class TPUTest {
 		 assertEquals(expected1, actual1);
 	
 	}
+	
+	@Test
+	public void testgetCheckInStatus() {
+		
+		Passenger testPilot = new Passenger("31251269","QA410",new Name("Bale","Tabina","Olvarado"),true,"Economy");
+		
+		String actualBookingRef = testPilot.getBookingReferenceNum();
+		String actualFlightCode = testPilot.getflightCode();
+		boolean actualCheckInStatus = testPilot.getCheckInStatus();
+		String actualClass = testPilot.getpClass(); 
+		
+		String expectedBookingref = "31251269";
+		String expectedFlightCode = "QA410";
+		boolean expectedCheckInStatus = true; 
+		String expectedClass = "Economy";
+		
+		assertEquals(expectedBookingref,actualBookingRef);
+		assertEquals(expectedFlightCode,actualFlightCode);
+		assertEquals(expectedCheckInStatus,actualCheckInStatus);
+		assertEquals(expectedClass,actualClass);
+		
+	}
+
+
+	@Test
+	public void testBaggage(){
+		Baggage testBag = new Baggage(12.0,22.0,32.0,42.0,new Passenger("31251269","QA410",new Name("Bale","Tabina","Olvarado"),true,"Economy"),new Flight("France","British Airways","BA808",150, null, null));
+		
+		Double actualWeight = testBag.getWeight();
+		Double actualLength = testBag.getHeight();
+		Double actualWidth = testBag.getWidth();
+		Double actualBreadth = testBag.getBreadth();
+		
+		Double expectedWeight = 12.0;
+		Double expectedLength = 22.0;
+		Double expectedWidth = 32.0;
+		Double expectedBreadth = 42.0;
+		
+		assertEquals(expectedWeight, actualWeight); 
+		assertEquals(expectedLength, actualLength); 
+		assertEquals(expectedWidth, actualWidth); 
+		assertEquals(expectedBreadth, actualBreadth); 
+
+	}
+	
+//	@Test
+//	void testExpectedException() {
+//		HashSet<Passenger> PassengerSet;
+//		PassengerSet = new HashSet<Passenger>();
+//		PassengerSet.add(new Passenger("31251269","QA410",new Name("Bale","Tabina","Olvarado"),true,"Economy"));
+//		PassengerSet.
+//		
+//		
+//	  Assertions.assertThrows(InvalidBookingReference.class, () -> (PassengerSet.), "Invalid name thus should throw");
+//	 
+//	}
 }
