ee3b290de33cba312387c3f6fd685d218baf22a2
rws1 <55705431+rws1@users.noreply.github.com>
2020-02-25 10:09:42 +0000

    Update Test

--- TPUTest.java
+++ TPUTest.java
@@ -1,29 +1,31 @@
 import java.lang.*;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 
-public class TPTest {
+public class TPUTest {
 
-	@test
+	@Test
 	public void testgetFullName() {
 		 String expected1 = "Robert William Stone";
 		 Name name1 = new Name("Robert","William","Stone");
 	     String actual1 = name1.getFullName();
 		 assertEquals(expected1, actual1);
 		 
-		 String expected1 = "Robert Stone";
-		 Name name1 = new Name("Robert","","Stone");
-	     String actual1 = name1.getFullName();
-		 assertEquals(expected1, actual1);
-	
-	
+		 String expected2 = "Robert Stone";
+		 Name name2 = new Name("Robert","","Stone");
+	     String actual2 = name2.getFullName();
+		 assertEquals(expected2, actual2);
 		 
-}
+	}
 
-	@test
+	@Test
 	public void testgetLastName() {
 		 String expected1 = "Stone";
 		 Name name1 = new Name("Robert","William","Stone");
 	     String actual1 = name1.getLastName();
 		 assertEquals(expected1, actual1);
 	
+	}
 }
