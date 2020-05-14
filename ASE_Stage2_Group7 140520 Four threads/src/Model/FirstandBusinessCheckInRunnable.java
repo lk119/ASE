59d6f663d6800abf7502af7086281be26c6a0b92
rrp3 <55551866+RachPatel@users.noreply.github.com>
2020-05-14 23:27:37 +0100

    FOUR THREADS WORK
    All 89 economy class passengers get checked in using 3 counters. The locks in 4 threads work well. 
    GUI is inactivated.

---
+++ ASE_Stage2_Group7 140520 Four threads/src/Model/FirstandBusinessCheckInRunnable.java
@@ -0,0 +1,48 @@
+package Model;
+
+import java.util.Observable;
+
+import View.GUIMain;
+
+public class FirstandBusinessCheckInRunnable extends Observable implements Runnable {
+
+	private GUIMain view1;
+	private boolean Finish = false;
+
+	PassengersInQueue q;
+
+	public FirstandBusinessCheckInRunnable(PassengersInQueue c) {
+		q = c;
+
+	}
+
+	public boolean isFinished() {
+		return Finish;
+	}
+	
+	//indicates end of auction
+	public void setFinished() {
+		Finish = true;
+
+	}
+	
+	@Override
+	public void run() {
+
+		Passenger pas = null;
+		
+			try {
+				Thread.sleep(1000);
+				
+				pas = q.BusinessFirstPicker();
+				//q.forFirstBusinessCheckin();
+				//q.forFirstBusinessCheckinLock();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+
+	}
+
---
+++ ASE_Stage2_Group7 140520 Four threads/src/Model/SecurityRunnable.java
@@ -0,0 +1,47 @@
+package Model;
+
+import java.util.Observable;
+
+import View.GUIMain;
+
+public class SecurityRunnable extends Observable implements Runnable {
+
+	private GUIMain view1;
+	private boolean Finish = false;
+
+	PassengersInQueue q;
+
+	public SecurityRunnable(PassengersInQueue c) {
+		q = c;
+
+	}
+
+	public boolean isFinished() {
+		return Finish;
+	}
+	
+	//indicates end of auction
+	public void setFinished() {
+		Finish = true;
+
+	}
+	
+
+	@Override
+	public void run() {
+		Passenger pas = null;
+		
+
+			try {
+				Thread.sleep(5000);
+				pas = q.PickerforSecurity();
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("SECURITY CHECK FINISHED");
+			Finish = true;
+		}
+	}
+
+
